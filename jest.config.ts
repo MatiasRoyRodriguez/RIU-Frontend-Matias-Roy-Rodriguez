e4c4d3988a55faab5d57deb44cbc2f17e8e5a75a
Matias Roy Rodriguez <matiasrr980@gmail.com>
2025-07-24 15:57:08 -0300

    chore(test): remove karma and configure jest as test runner

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!flat)/',
+  ],
+};
+
+export default config;
