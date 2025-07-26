a61851d51e45b997db23a4e684898902d31e2ac7
Matias Roy Rodriguez <matiasrr980@gmail.com>
2025-07-26 11:42:22 -0300

    feat(hero-edit-page): add hero editpage with form integration

---
+++ src/app/features/heroes/pages/hero-edit-page/hero-edit-page.ts
@@ -0,0 +1,51 @@
+import { Component, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+
+import { map, Observable, switchMap } from 'rxjs';
+
+import { HeroService } from '../../services/hero.service';
+import { Hero } from '../../models/hero.model';
+import { HeroFormComponent } from '../../components/hero-form/hero-form.component';
+
+@Component({
+  selector: 'app-hero-edit-page',
+  imports: [
+    HeroFormComponent,
+    MatProgressSpinner
+  ],
+  templateUrl: './hero-edit-page.html',
+  styleUrl: './hero-edit-page.scss'
+})
+export class HeroEditPage {
+
+  loading = false;
+  private readonly route = inject(ActivatedRoute);
+  private readonly heroService = inject(HeroService);
+  private readonly router = inject(Router);
+
+  private heroId$: Observable<number> = this.route.paramMap.pipe(
+    map(params => {
+      const raw = params.get('id');
+      if (!raw) throw new Error('ID param not found');
+      return Number(raw);
+    })
+  );
+
+  readonly hero: Signal<Hero | undefined> = toSignal(
+    this.heroId$.pipe(
+      switchMap(id => this.heroService.getHeroById(id))
+    ),
+    { initialValue: undefined }
+  );
+
+  onSubmit(hero: Hero): void {
+    this.loading = true;
+    this.heroService.updateHero(hero).subscribe({
+      next: () => this.router.navigateByUrl(''),
+      complete: () => (this.loading = false)
+    });
+  }
+
+}
