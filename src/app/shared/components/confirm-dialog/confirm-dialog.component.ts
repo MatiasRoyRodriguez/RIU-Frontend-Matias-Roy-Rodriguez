db96a00b10b8641e6ffab62eedf228112b7095de
Matias Roy Rodriguez <matiasrr980@gmail.com>
2025-07-26 14:35:24 -0300

    feat(confirm-dialog): add confirm dialog modal

---
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,26 @@
+import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+  ],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss'
+})
+export class ConfirmDialogComponent {
+  dialogRef = inject(MatDialogRef<ConfirmDialogComponent>);
+  data = inject(MAT_DIALOG_DATA);
+
+  onConfirm() {
+    this.dialogRef.close(true);
+  }
+
+  onCancel() {
+    this.dialogRef.close(false);
+  }
+}
