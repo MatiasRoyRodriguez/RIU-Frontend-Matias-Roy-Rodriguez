68a184a321e40a177e7f16dcd497960ec82bac1c
Matias Roy Rodriguez <matiasrr980@gmail.com>
2025-07-26 16:37:16 -0300

    feat(hero-list-page): add pagination

--- src/app/features/heroes/pages/hero-list/hero-list.page.ts
+++ src/app/features/heroes/pages/hero-list/hero-list.page.ts
@@ -14,6 +14,10 @@ import { MatInputModule } from '@angular/material/input';
 import { HeroService } from '../../services/hero.service';
 import { HeroCardComponent } from '../../components/hero-card/hero-card.component';
 import { confirmAndDeleteHero } from '../../../../shared/utils/hero-utils';
+
+const PAGE_SIZE = 5;
+
+
 @Component({
   selector: 'app-hero-list',
   imports: [
@@ -41,21 +45,54 @@ export class HeroListPage {
   readonly allHeroes = this.heroService.getHeroesSignal();
   readonly search = signal('');
   readonly searchControl = new FormControl('');
+  readonly currentPage = signal(1);
 
-  readonly heroes = computed(() => {
+  readonly filteredHeroes = computed(() => {
     const query = this.search().toLowerCase();
-    return this.allHeroes().filter(hero => hero.name.toLowerCase().includes(query));
+    return this.allHeroes().filter(hero =>
+      hero.name.toLowerCase().includes(query)
+    );
   });
+
+  readonly totalPages = computed(() =>
+    Math.max(1, Math.ceil(this.filteredHeroes().length / PAGE_SIZE))
+  );
+
+  readonly paginatedHeroes = computed(() => {
+    const start = (this.currentPage() - 1) * PAGE_SIZE;
+    return this.filteredHeroes().slice(start, start + PAGE_SIZE);
+  });
+
 
   constructor() {
     effect(() => {
       const sub = this.searchControl.valueChanges.subscribe(value => {
         this.search.set(value ?? '');
+        this.currentPage.set(1);
       });
       return () => sub.unsubscribe();
     });
+    effect(() => {
+      const page = this.currentPage();
+      const max = this.totalPages();
+
+      if (page > max) {
+        this.currentPage.set(max);
+      }
+    });
   }
 
+  nextPage() {
+    if (this.currentPage() < this.totalPages()) {
+      this.currentPage.update(p => p + 1);
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage() > 1) {
+      this.currentPage.update(p => p - 1);
+    }
+  }
 
   onDelete(id: number): void {
     confirmAndDeleteHero(this.dialog, this.snackBar, this.heroService, id, () => {
