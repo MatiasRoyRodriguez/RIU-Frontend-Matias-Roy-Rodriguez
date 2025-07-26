c6b45ad411089e45ff3c0a15c507fb30fd7f519c
Matias Roy Rodriguez <matiasrr980@gmail.com>
2025-07-26 11:06:37 -0300

    feat(hero-list-page): add navigation button to create new hero

--- src/app/features/heroes/pages/hero-list/hero-list.page.ts
+++ src/app/features/heroes/pages/hero-list/hero-list.page.ts
@@ -1,8 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { HeroService } from '../../services/hero.service';
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {  RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { HeroService } from '../../services/hero.service';
 import { HeroCardComponent } from '../../components/hero-card/hero-card.component';
 @Component({
   selector: 'app-hero-list',
@@ -10,7 +14,10 @@ import { HeroCardComponent } from '../../components/hero-card/hero-card.componen
     AsyncPipe,
     MatCardModule,
     MatProgressSpinnerModule,
-    HeroCardComponent
+    HeroCardComponent,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './hero-list.page.html',
   styleUrl: './hero-list.page.scss',
